0b9bfc2c6262f96fb88365d2a0690252879b32e2
Roshankumar0808 <roshananand2014@gmail.com>
2025-02-20 14:47:11 +0530

    Spring Security Part1: Initializing

---
+++ src/main/java/com/SpringSecurityPart1/SpringSecurityPart1/SpringSecurityPart1Application.java
@@ -0,0 +1,13 @@
+package com.SpringSecurityPart1.SpringSecurityPart1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPart1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityPart1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringSecurityPart1/SpringSecurityPart1/SpringSecurityPart1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringSecurityPart1.SpringSecurityPart1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPart1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
